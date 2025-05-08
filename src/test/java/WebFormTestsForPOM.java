5d9ebab6abd38d4d6cb6a4464bb090fb6881c044
ISmeneger <ISmeneger@yandex.ru>
2025-05-08 20:26:34 +0300

    Add WebForm tests for POM

--- src/test/java/WebFormTestsForPOM.java
+++ src/test/java/WebFormTestsForPOM.java
@@ -2,14 +2,10 @@ import Ilya_S.pageObjects.HomePage;
 import Ilya_S.pageObjects.WebFormPage;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.WebElement;
 
 import static Ilya_S.pageObjects.HomePage.BASE_URL;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Feature("POM")
 class WebFormTestsForPOM extends BaseTestForPOM {
@@ -40,26 +36,5 @@ class WebFormTestsForPOM extends BaseTestForPOM {
 
         webFormPage.submitForm();
         Thread.sleep(3000);
-    }
-
-    @Test
-    void checkTextareaInput() {
-        driver.findElement(By.linkText("Web form")).click();
-
-        WebElement textarea = driver.findElement(By.name("my-textarea"));
-        textarea.sendKeys("Test\nTest2");
-        driver.findElement(By.xpath("//button[text()='Submit']")).click();
-        WebElement h1 = driver.findElement(By.xpath("//h1[text()='Form submitted']"));
-        assertEquals("Form submitted", h1.getText());
-    }
-
-    @Test
-    void disabledInput() {
-        driver.findElement(By.linkText("Web form")).click();
-
-        WebElement disabled = driver.findElement(By.name("my-disabled"));
-        assertEquals("", disabled.getText());
-        Exception thrown = assertThrows(ElementNotInteractableException.class, () -> disabled.sendKeys("Test\n"));
-        assertThat(thrown.getMessage()).contains("element not interactable");
     }
 }

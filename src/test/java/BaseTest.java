25eff9b5771a0eb26cd54b92cc5fe5e5cb5310cd
ISmeneger <ISmeneger@yandex.ru>
2025-05-10 20:21:53 +0300

    Add initDriver

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,9 +1,15 @@
+import io.qameta.allure.Allure;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.Map;
 
 class BaseTest {
     WebDriver driver;
@@ -11,7 +17,7 @@ class BaseTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        initDriver();
         driver.get(BASE_URL);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
@@ -20,5 +26,25 @@ class BaseTest {
     @AfterEach
     void tearDown() {
         driver.quit();
+    }
+
+    private void initDriver() {
+        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+        Allure.addAttachment("remoteUrl", remoteUrl);
+        if (remoteUrl != null || !remoteUrl.isEmpty()) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");  // Add headless mode
+            options.addArguments("--disable-gpu"); // Switch off GPU, because we don't need it in headless mode
+            options.addArguments("--no-sandbox"); // Switch off sandbox to prevent access rights issues
+            options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
+            options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+            try {
+                driver = new RemoteWebDriver(new URL(remoteUrl), options);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
+            }
+        } else {
+            driver = new ChromeDriver();
+        }
     }
 }
